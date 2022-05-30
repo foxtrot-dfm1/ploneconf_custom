f05e34732368829e269e2dd511ab4645adcf2b69
roman <kysilroman99@gmail.com>
2022-05-30 21:56:25 +0200

    voting removed; TODO: add voting as behavior

--- src/ploneconf/site/content/sponsor.py
+++ src/ploneconf/site/content/sponsor.py
@@ -6,6 +6,7 @@ from plone.supermodel import model
 from plone.supermodel.directives import fieldset
 from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
+from zope.interface import implementer
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
